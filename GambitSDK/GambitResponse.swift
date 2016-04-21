d6ca136c9ecf8537f57f0fb4889bb90053e75e43
Joel Edwards <jedwards@aviatainc.com>
2016-04-21 13:46:54 -0600

    GAM-1491
    README.md
    - pulled in a few changes from to the README
    
    GambitSDK/GambitResponse.swift
    - merged in some message payload inspection and formatting code

--- GambitSDK/GambitResponse.swift
+++ GambitSDK/GambitResponse.swift
@@ -35,5 +35,101 @@ public class GambitResponseMessage: GambitResponse {
   }
 }
 
+public class GambitMessageResponse: GambitResponse, CustomStringConvertible {
+  public let namespace: String
+  public let campaignID: Int
+  public let campaignName: String
+  public let ciidHash: String
+  public let eventName: String
+  public let messageID: String
+  public var forwardedEvent: [String: AnyObject]?
+  public var notificationMsg: String?
+  
+  public var accessKey: String?
+  public var clientSalt: String?
+  public var timestamp: String?
+  public var debugDirective: String?
+  public var attributes: [String: AnyObject]?
+  
+  public required init(json: JSON) throws {
+    guard let msg = json["message"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    let j = try NSJSONSerialization.JSONObjectWithData(msg.dataUsingEncoding(NSUTF8StringEncoding)!, options: .AllowFragments)
+    
+    guard let namespace = j["namespace"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    guard let campaignID = j["campaign_id"] as? Int else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    guard let campaignName = j["campaign_name"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    guard let ciidHash = j["ciid_hash"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    guard let eventName = j["event_name"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+    guard let messageID = j["message_id"] as? String else {
+      throw NSError(domain: "GambitSDKError - ResponseMessage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Bad JSON"])
+    }
+
+    self.namespace      = namespace
+    self.campaignID     = campaignID
+    self.campaignName   = campaignName
+    self.ciidHash       = ciidHash
+    self.eventName      = eventName
+    self.messageID      = messageID
+    self.forwardedEvent = j["forwarded_event"] as? [String: AnyObject]
+    self.notificationMsg = j["notification_message"] as? String
+    
+    if let dataString = j["data"] as? String {
+      let d = try NSJSONSerialization.JSONObjectWithData(dataString.dataUsingEncoding(NSUTF8StringEncoding)!, options: .AllowFragments)
+      
+      self.accessKey      = d["access_key"] as? String
+      self.clientSalt     = d["client_salt"] as? String
+      self.timestamp      = d["timestamp"] as? String
+      self.debugDirective = d["debug_directive"] as? String
+      self.attributes     = d["attributes"] as? [String: AnyObject]
+    }
+  }
+  
+  public var description: String {
+    var s = ""
+    
+    s += "namespace: \(self.namespace)\n"
+    s += "campaign_id: \(self.campaignID)\n"
+    s += "campaign_name: \(self.campaignName)\n"
+    s += "ciid_hash: \(self.ciidHash)\n"
+    s += "event_name: \(self.eventName)\n"
+    s += "message_id: \(self.messageID)\n"
+    if let forwardedEvent = self.forwardedEvent {
+      s += "forwarded_event: \(forwardedEvent)"
+    }
+    if let notificationMsg = self.notificationMsg {
+      s += "notification_message: \(notificationMsg)"
+    }
+    if let accessKey = self.accessKey {
+      s += "access_key: \(accessKey)\n"
+    }
+    if let clientSalt = self.clientSalt {
+      s += "client_salt: \(clientSalt)\n"
+    }
+    if let timestamp = self.timestamp {
+      s += "timestamp: \(timestamp)\n"
+    }
+    if let debugDirective = self.debugDirective {
+      s += "debug_directive: \(debugDirective)\n"
+    }
+    if let attributes = self.attributes {
+      s += "attributes: \(attributes)\n"
+    }
+    
+    return s
+  }
+}
+
 public typealias GambitResponseEvent = GambitResponseMessage
 public typealias GambitResponsePush = GambitResponseMessage
--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# Cogs iOS SDK
+# Cogs iOS Example App for Cogs SDK
 
-For the time being, see documentation on https://www.cogswell.io
+## Description
+The Example app for the Cogs SDK real-time message brokering system
+
+## Requirements
+
+* iOS 9.0+
+* Xcode 7.3+
+* Requires APNs enabled application. You can read more about APNs [here](https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/IPhoneOSClientImp.html#//apple_ref/doc/uid/TP40008194-CH103-SW2)
+
+## Installation
+### Manual
+* Check out or download Cogs SDK
+* Open GambitSDK.xcodeproj in Xcode
+* Build the framework
+* Navigate to the derived data folder and copy GambitSDK.framework and add it to you project
+
+You can view a demo example using the SDK here: https://github.com/cogswell-io/cogs-ios-client-sdk
+
+### Submodule
+
+* Open up Terminal, and cd into your top-level project directory. If your project is not initialized as a git repository then run the following command:
+```javascript
+$ git init
+```
+* Add Cogs SDK as a git submodule by running the following command:
+```javascript
+$ git submodule add https://github.com/cogswell-io/cogs-ios-client-sdk
+```
+* Open the new GambitSDK folder, and drag the GambitSDK.xcodeproj into the Project Navigator of your application's Xcode project.
+* Select the GambitSDK.xcodeproj in the Project Navigator and verify the deployment target matches that of your application target.
+* Select your application project in the Project Navigator (blue project icon) to navigate to the target configuration window and select the application target under the "Targets" heading in the sidebar.
+* In the tab bar at the top of that window, open the "General" panel.
+* Click on the + button under the "Embedded Binaries" section.
+* Select the GambitSDK.framework for iOS
 
 ## [Code Samples](#code-samples)
 You will see the name Gambit throughout our code samples. This was the code name used for Cogs prior to release.
@@ -213,3 +246,18 @@ cogsService.message(messageRequest) { dat, rsp, err in
 }
 ```
 
+## License
+
+Copyright 2016 Aviata Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
